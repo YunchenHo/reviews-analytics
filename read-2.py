cb32aa4f9268306f19b1f1504da6aacbb7cde9d1
Yunchen Ho <Janet20050705@gmail.com>
2020-06-23 22:18:50 +0800

    calc avg len of reviews

--- read-2.py
+++ read-2.py
@@ -6,8 +6,9 @@ with open('original.txt', 'r') as f:
 		count += 1 #count=count+1
 		if count % 1000 == 0: #%=餘數
 			print(len(data))
-print(len(data))
-
-print(data[0])
-print('------------')
-print(data[1])
+print('檔案讀取完了，總共有', len(data) ,'筆資料')
+		
+sum_len = 0 #總長度
+for d in data: #因為已將line存入data清單 清單才能算字串長度
+	sum_len = sum_len + len(d)
+print('留言的平均長度是', sum_len / len(data))
